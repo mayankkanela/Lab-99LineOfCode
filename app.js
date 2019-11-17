cf624e0e529005c981bcf48a32f10f1c70503257
mayankkanela <mayankkanela8@gmail.com>
2019-11-17 16:14:15 +0530

    'final'

---
+++ app.js
@@ -0,0 +1,14 @@
+let friends =['Jon','Doe','Sven','Felix','Bravo']
+var i=0;
+while(friends[i]!=null)
+{
+    console.log(friends[i]+":");
+    for(var j=99;j>0;j--)
+    {
+        if(j==1)
+        console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out, no more lines of code');  
+        else
+        console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out'+--j+' lines of code');   
+    }
+        i++;
+}
