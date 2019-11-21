b679b533862c3afcaa2ead8ad061a728f4e7f981
mayankkanela <mayankkanela8@gmail.com>
2019-11-22 03:11:28 +0530

    final commit

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ button.addEventListener('click',function()
     {  
         while(friends[i]!=null)
         {
-            console.log(friends[i]+":");
+            let friend =document.createElement('div');
+            friend.className='friend';
+            document.body.appendChild(friend);
+            let heading=document.createElement("h3");
+            let name=document.createTextNode(friends[i]); 
+            heading.appendChild(name);
+            friend.appendChild(heading);
+           // console.log(friends[i]+":");
             for(var j=99;j>0;j--)
             {
+                let para=document.createElement('p');
                 if(j==1)
-                console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out, no more lines of code');  
+                {
+                    para.innerHTML=j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out, no more lines of code';
+                friend.appendChild(para);
+            //    console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out, no more lines of code');  
+                 }
                 else
-                console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out'+--j+' lines of code');   
+                {
+                    para.innerHTML=j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out'+(j-1)+' lines of code';
+                    friend.appendChild(para);
+              //      console.log(j+' lines of code in the file,'+j+' lines of code;'+friends[i]+' strikes one out, clear it all out'+--j+' lines of code');   
+                }
             }
                 i++;
         }
